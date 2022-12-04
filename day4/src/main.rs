778ef6eed1c70cd53ba24845cebaef869414e292
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2022-12-04 17:08:52 +0100

    day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -10,23 +10,26 @@ fn main() {
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("part1 {}", part1(&input));
-    // println!("part2 {}", part2(&input))
+    println!("part2 {}", part2(&input))
 }
 
 fn part1(input: &str) -> usize {
     input
         .lines()
         .map(|line| {
-            let (left, right) = line.split_once(",").unwrap();
-
-            let left_sections = parse_assignment(left);
-            let right_sections = parse_assignment(right);
-
-            // println!("{}: {:?},{:?}", line, left_sections, right_sections);
-            (left_sections.is_subset(&right_sections) || left_sections.is_superset(&right_sections))
-                as usize // bool is guaranteed to be 0 or 1: https://doc.rust-lang.org/std/primitive.bool.html
+            let (left, right) = parse_line(line);
+            // println!("{}: {:?},{:?}", line, left, right);
+            (left.is_subset(&right) || left.is_superset(&right)) as usize // bool is guaranteed to be 0 or 1: https://doc.rust-lang.org/std/primitive.bool.html
         })
         .sum()
+}
+
+fn parse_line(line: &str) -> (HashSet<i32>, HashSet<i32>) {
+    let (left, right) = line.split_once(",").unwrap();
+
+    let left_sections = parse_assignment(left);
+    let right_sections = parse_assignment(right);
+    (left_sections, right_sections)
 }
 
 fn parse_assignment(assignment: &str) -> HashSet<i32> {
@@ -34,6 +37,13 @@ fn parse_assignment(assignment: &str) -> HashSet<i32> {
     (from.parse::<i32>().unwrap()..to.parse::<i32>().unwrap() + 1).collect()
 }
 
-// fn part2(input: &str) -> usize {
-//     0
-// }
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (left, right) = parse_line(line);
+            // println!("{}: {:?},{:?}", line, left, right);
+            (left.intersection(&right).count() > 0) as usize // bool is guaranteed to be 0 or 1: https://doc.rust-lang.org/std/primitive.bool.html
+        })
+        .sum()
+}
