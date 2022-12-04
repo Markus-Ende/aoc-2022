801903ffee24a953d6ade927e209408f565f665b
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2022-12-04 17:00:08 +0100

    day4 part1

---
+++ day4/src/main.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    // println!("Input file {}", file_path);
+
+    let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("part1 {}", part1(&input));
+    // println!("part2 {}", part2(&input))
+}
+
+fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once(",").unwrap();
+
+            let left_sections = parse_assignment(left);
+            let right_sections = parse_assignment(right);
+
+            // println!("{}: {:?},{:?}", line, left_sections, right_sections);
+            (left_sections.is_subset(&right_sections) || left_sections.is_superset(&right_sections))
+                as usize // bool is guaranteed to be 0 or 1: https://doc.rust-lang.org/std/primitive.bool.html
+        })
+        .sum()
+}
+
+fn parse_assignment(assignment: &str) -> HashSet<i32> {
+    let (from, to) = assignment.split_once("-").unwrap();
+    (from.parse::<i32>().unwrap()..to.parse::<i32>().unwrap() + 1).collect()
+}
+
+// fn part2(input: &str) -> usize {
+//     0
+// }
