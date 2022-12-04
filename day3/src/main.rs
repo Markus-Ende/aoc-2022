04a93c0b7e6526389f89f7166d57f52df5e93573
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2022-12-04 15:18:25 +0100

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::env;
 use std::fs;
 
@@ -9,26 +10,58 @@ fn main() {
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("part1 {}", part1(&input));
+    println!("part2 {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
     let lines = input.lines();
     lines
         .map(|line| {
-            let left = &line[..line.len() / 2];
-            let right = &line[line.len() / 2..];
+            let (left, right) = compartments(line);
             let duplicate_index =
                 right.find(|char| left.find(|leftchar| leftchar == char).is_some());
 
             match duplicate_index {
-                Some(index) => {
-                    // println!("found {}", right.chars().nth(index).unwrap());
-                    " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                        .find(right.chars().nth(index).unwrap())
-                        .unwrap()
-                }
+                Some(index) => priority(right.chars().nth(index).unwrap()),
                 _ => panic!("has no duplicate char: {}", line),
             }
         })
         .sum()
+}
+
+fn part2(input: &str) -> usize {
+    let lines = input.lines();
+    lines
+        .map(|rucksack| rucksack.chars().unique().join(""))
+        .tuples::<(_, _, _)>()
+        .map(|rucksacks| {
+            // TODO: find a better way to map the items to avoid joining multiple times
+            let joined_rucksacks = [rucksacks.0, rucksacks.1, rucksacks.2].into_iter().join("");
+            let x = joined_rucksacks.chars().find(|char| {
+                joined_rucksacks
+                    .chars()
+                    .filter(|c| char == c)
+                    .join("")
+                    .len()
+                    == 3
+            });
+            match x {
+                Some(char) => priority(char),
+                _ => panic!("has no char in common: {}", joined_rucksacks),
+            }
+        })
+        .sum()
+}
+
+fn compartments(rucksack: &str) -> (&str, &str) {
+    (
+        &rucksack[..rucksack.len() / 2],
+        &rucksack[rucksack.len() / 2..],
+    )
+}
+
+fn priority(char: char) -> usize {
+    " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .find(char)
+        .unwrap()
 }
