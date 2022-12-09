8c6e02d7d4f54d3b5194372db0ee5959015ea687
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2022-12-09 09:43:06 +0100

    day9 part2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    env, fs,
-};
+use std::{collections::HashSet, env, fs, iter};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,8 +7,9 @@ fn main() {
 
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    println!("part1 {}", part1(&input));
-    println!("part2 {}", part2(&input));
+    println!("part1    {}", part1(&input));
+    println!("part1 v2 {}", part1_v2(&input));
+    println!("part2    {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -43,6 +41,46 @@ fn part1(input: &str) -> usize {
     visited.len()
 }
 
-fn part2(_: &str) -> usize {
-    0
+fn part1_v2(input: &str) -> usize {
+    simulate_rope(input, 2)
+}
+
+fn part2(input: &str) -> usize {
+    simulate_rope(input, 10)
+}
+
+fn simulate_rope(input: &str, size: usize) -> usize {
+    let mut visited = HashSet::<(i32, i32)>::new();
+    let mut segments = iter::repeat((0, 0)).take(size).collect::<Vec<(_, _)>>();
+    visited.insert((0, 0));
+
+    for line in input.lines() {
+        let (direction, steps) = line.split_once(" ").unwrap();
+        for _ in 0..steps.parse().unwrap() {
+            let head = segments.get_mut(0).unwrap();
+            match direction {
+                "U" => head.1 -= 1,
+                "R" => head.0 += 1,
+                "D" => head.1 += 1,
+                "L" => head.0 -= 1,
+                _ => panic!("unknown direction"),
+            };
+            for i in 1..segments.len() {
+                let head_to_compare = segments[i - 1];
+                let tail_to_compare = segments.get_mut(i).unwrap();
+                let dx = head_to_compare.0 - tail_to_compare.0 as i32;
+                let dy = head_to_compare.1 - tail_to_compare.1 as i32;
+
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    tail_to_compare.0 += dx.signum();
+                    tail_to_compare.1 += dy.signum();
+                }
+            }
+            visited.insert(*segments.last().unwrap());
+
+            // println!("{:?}", segments);
+        }
+    }
+    // println!("{:?}", visited);
+    visited.len()
 }
