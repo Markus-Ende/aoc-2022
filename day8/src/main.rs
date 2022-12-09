38156696a054db19577df4fcfe05fcdd0ab46c37
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2022-12-09 08:03:24 +0100

    day8 part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -63,5 +63,54 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut max_score = 0;
+
+    for x in 1..map[0].len() - 1 {
+        for y in 1..map.len() - 1 {
+            let mut tree_count_up = 0;
+            for dy in (0..y).rev() {
+                tree_count_up += 1;
+                if map[dy][x] >= map[y][x] {
+                    break;
+                }
+            }
+
+            let mut tree_count_down = 0;
+            for dy in (y + 1)..map.len() {
+                tree_count_down += 1;
+                if map[dy][x] >= map[y][x] {
+                    break;
+                }
+            }
+
+            let mut tree_count_left = 0;
+            for dx in (0..x).rev() {
+                tree_count_left += 1;
+                if map[y][dx] >= map[y][x] {
+                    break;
+                }
+            }
+
+            let mut tree_count_right = 0;
+            for dx in ((x + 1)..map.len()) {
+                tree_count_right += 1;
+                if map[y][dx] >= map[y][x] {
+                    break;
+                }
+            }
+
+            let score = tree_count_up * tree_count_down * tree_count_left * tree_count_right;
+            if score > max_score {
+                max_score = score;
+            }
+
+            println!(
+                "({},{}): {} (up {}, right {}, down {}, left {})",
+                x, y, score, tree_count_up, tree_count_right, tree_count_down, tree_count_left
+            );
+        }
+    }
+
+    max_score
 }
