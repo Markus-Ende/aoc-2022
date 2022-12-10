153853e45110e884c66bbcd87aa0ca93edf9ce51
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2022-12-10 11:05:31 +0100

    day10 part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("part1    {}", part1(&input));
-    println!("part2    {}", part2(&input));
+    println!("part2:");
+    part2(&input);
 }
 
 fn part1(input: &str) -> i32 {
@@ -21,7 +22,7 @@ fn part1(input: &str) -> i32 {
             ["noop"] => {
                 cycles += 1;
                 if [20, 60, 100, 140, 180, 220].contains(&cycles) {
-                    println!("cycle {}, x {}", cycles, x);
+                    // println!("cycle {}, x {}", cycles, x);
                     signal_strength += cycles * x;
                 }
             }
@@ -29,7 +30,7 @@ fn part1(input: &str) -> i32 {
                 cycles += 2;
                 if [20, 21, 60, 61, 100, 101, 140, 141, 180, 181, 220, 221].contains(&cycles) {
                     let real_cycle = if cycles % 2 == 0 { cycles } else { cycles - 1 };
-                    println!("cycle {}, x {}", real_cycle, x);
+                    // println!("cycle {}, x {}", real_cycle, x);
                     signal_strength += real_cycle * x;
                 }
                 x += arg.parse::<i32>().unwrap();
@@ -40,6 +41,42 @@ fn part1(input: &str) -> i32 {
     signal_strength
 }
 
-fn part2(_: &str) -> usize {
-    0
+fn part2(input: &str) -> () {
+    let mut x = 1;
+    let mut cycles = 0;
+
+    input.lines().for_each(|line| {
+        let instruction: Vec<_> = line.split_ascii_whitespace().collect();
+        match &instruction[..] {
+            ["noop"] => {
+                draw(x, cycles);
+                cycles += 1;
+                check_newline(cycles);
+            }
+            ["addx", arg] => {
+                draw(x, cycles);
+                cycles += 1;
+                check_newline(cycles);
+                draw(x, cycles);
+                cycles += 1;
+                check_newline(cycles);
+                x += arg.parse::<i32>().unwrap();
+            }
+            _ => panic!("unknown instruction"),
+        }
+    });
+}
+
+fn draw(x: i32, cycle: i32) -> () {
+    if [x - 1, x, x + 1].contains(&(cycle % 40)) {
+        print!("#");
+    } else {
+        print!(".");
+    }
+}
+
+fn check_newline(cycle: i32) -> () {
+    if [40, 80, 120, 160, 200, 240].contains(&cycle) {
+        println!();
+    }
 }
